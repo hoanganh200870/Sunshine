d6d7a6f62818893ffe37393b47941619dca229d1
Hoàng Anh <hoanganh200870@gmail.com>
2015-01-18 23:23:40 +0700

    Inflate menu for ForecastFragment

--- app/src/main/java/hcmut/hoanganh/sunshine/ForecastFragment.java
+++ app/src/main/java/hcmut/hoanganh/sunshine/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -27,6 +30,32 @@ import java.util.ArrayList;
 public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecast_fragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_refresh:
+                new FetchWeatherTask().execute();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
